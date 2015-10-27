7df528af9fe599e06da88d56458fd840f3d20787
RikyDev <westrada-cc@conestogac.on.ca>
2015-10-27 15:55:13 +0000

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     validates(:name, presence: true, length: { maximum: 50 })
@@ -29,11 +29,6 @@ class User < ActiveRecord::Base
         update_attribute(:remember_digest, User.digest(remember_token))
     end
     
-    # Returns true if the given token matches the digest.
-    #def authenticated?(remember_token)
-    #    return false if remember_digest.nil?
-    #    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    #end
     
     # Returns true if the given token matches the digest.
     def authenticated?(attribute, token)
@@ -59,6 +54,24 @@ class User < ActiveRecord::Base
         UserMailer.account_activation(self).deliver_now
     end
     
+    
+    # Sets the password reset attributes.
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest, User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+    
+    # Sends password reset email.
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+    
+    
+    # Returns true if a password reset has expired.
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
     
     private 
     
